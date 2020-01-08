df62571c9141ce26456d2f244fd4955e26af0403
dflk <904885508@qq.com>
2020-01-08 22:05:08 +0800

    从零开始创建并使用ORM连接数据库

--- src/main/java/hello/configuration/JavaConfiguration.java
+++ src/main/java/hello/configuration/JavaConfiguration.java
@@ -1,6 +1,8 @@
 package hello.configuration;
 
+import hello.mapper.UserMapper;
 import hello.service.OrderService;
+import hello.service.User;
 import hello.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +11,13 @@ import org.springframework.context.annotation.Configuration;
 public class JavaConfiguration {
 
     @Bean
-    public UserService userService() {
-        return  new UserService();
+    public UserService userService(UserMapper userMapper) {
+        return  new UserService(userMapper);
     }
 
     @Bean
-    public OrderService orderService() {
-        return new OrderService(new UserService());
+    public OrderService orderService(UserMapper userMapper) {
+        return new OrderService(new UserService(userMapper));
     }
 
 }
