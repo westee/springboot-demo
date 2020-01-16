f28ee103a4fbf0ee1f50f6afd05c0f37a20d9f74
dflk <904885508@qq.com>
2020-01-16 21:59:31 +0800

    完成登录模块

--- src/main/java/hello/controller/AuthController.java
+++ src/main/java/hello/controller/AuthController.java
@@ -20,7 +20,7 @@ public class AuthController {
     private UserService userService;
 
     @Inject
-    public AuthController( AuthenticationManager authenticationManager, UserService userService) {
+    public AuthController(AuthenticationManager authenticationManager, UserService userService) {
         this.authenticationManager = authenticationManager;
         this.userService = userService;
     }
@@ -36,26 +36,63 @@ public class AuthController {
     public Object auth() {
         String userName = SecurityContextHolder.getContext().getAuthentication().getName();
 
-        User loggedInUser =  userService.getUserByUsername(userName);
+        User loggedInUser = userService.getUserByUsername(userName);
 
-        if(loggedInUser == null){
+        if (loggedInUser == null) {
             return new Result("ok", "用户未登录", false);
         } else {
-            return new Result("ok", null, true, loggedInUser);
+            return new Result("ok", "用户已登录", true, loggedInUser);
+        }
+    }
+
+    @PostMapping("/auth/register")
+    @ResponseBody
+    public Result register(@RequestBody Map<String, String> usernameAndPassword) {
+        String username = usernameAndPassword.get("username");
+        String password = usernameAndPassword.get("password");
+        if (username == null || password == null) {
+            return new Result("fail", "username/password == null", false);
         }
 
+        if (username.length() < 6 || username.length() > 15) {
+            return new Result("fail", "invalid username", false);
+        }
+
+        if (password.length() < 6 || password.length() > 15) {
+            return new Result("fail", "invalid password", false);
+        }
+
+        User user = userService.getUserByUsername(username);
+
+        if (user == null) {
+            userService.save(username, password);
+            return new Result("ok", "success", false);
+        } else {
+            return new Result("fail", "用户已经存在", false);
+        }
+    }
+
+    @GetMapping("/auth/logout")
+    @ResponseBody
+    public Object logout(@RequestParam String username){
+        User loggedInUser = userService.getUserByUsername(username);
+        if(loggedInUser == null){
+            return new Result("fail","用户没有登陆",false);
+        } else {
+            return new Result("ok", "注销成功", false);
+        }
     }
 
     @PostMapping("/auth/login")
     @ResponseBody
-    public Result login(@RequestBody Map<String, String> usernameAndPasswordJson){
+    public Result login(@RequestBody Map<String, String> usernameAndPasswordJson) {
         String username = usernameAndPasswordJson.get("username");
         String password = usernameAndPasswordJson.get("password");
         UserDetails userDetails;
-        try{
-             userDetails = userService.loadUserByUsername(username);
-        } catch (UsernameNotFoundException e){
-            return new Result("fail","用户不存在", false);
+        try {
+            userDetails = userService.loadUserByUsername(username);
+        } catch (UsernameNotFoundException e) {
+            return new Result("fail", "用户不存在", false);
         }
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userDetails, password, userDetails.getAuthorities());
 
@@ -64,7 +101,7 @@ public class AuthController {
             SecurityContextHolder.getContext().setAuthentication(token);
 
             return new Result("ok", "登录成功", true, userService.getUserByUsername(username));
-        } catch (BadCredentialsException e){
+        } catch (BadCredentialsException e) {
             return new Result("fail", "密码不正确", false);
         }
     }
