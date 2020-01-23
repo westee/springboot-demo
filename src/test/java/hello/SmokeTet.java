9e2d67ee645de79edfba7a5f6af7fbc78139f78c
dflk <904885508@qq.com>
2020-01-23 20:41:56 +0800

    开始编写自动化测试

---
+++ src/test/java/hello/SmokeTet.java
@@ -0,0 +1,10 @@
+package hello;
+
+import org.junit.jupiter.api.Test;
+
+public class SmokeTet {
+    @Test
+    public void test(){
+
+    }
+}
---
+++ src/test/java/hello/service/UserServiceTest.java
@@ -0,0 +1,50 @@
+package hello.service;
+
+import hello.entity.User;
+import hello.mapper.UserMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+    @Mock
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Mock
+    UserMapper userMapper;
+
+    @InjectMocks
+    UserService userService = new UserService(bCryptPasswordEncoder, userMapper);
+
+    @Test
+    public void testSave() {
+        Mockito.when(bCryptPasswordEncoder.encode("password")).thenReturn("encodePassword");
+        userService.save("user", "password");
+        Mockito.verify(userMapper).save("user", "encodePassword");
+    }
+
+    @Test
+    public void testGetUserByUsername() {
+        userService.getUserByUsername("user");
+
+        Mockito.verify(userMapper).findUserByUsername("user");
+    }
+
+    @Test
+    public void testLoadUserByUsernameFailed() {
+        Assertions.assertThrows(UsernameNotFoundException.class,
+                () -> userService.loadUserByUsername("myUser"));
+    }
+
+    @Test
+    public void testLoadUserByUsernameSuccess() {
+        Mockito.when(userMapper.findUserByUsername("user"))
+                .thenReturn(new User(123, "user","encodePassword"));
+    }
+}
